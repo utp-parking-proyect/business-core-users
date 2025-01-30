7bf216940e5921b75703e7e49c313e718a7cfb31
sebastian-reyes <sebastianreyes2105@gmail.com>
2025-01-30 01:34:48 -0500

    feature: add models to user service and connection to BD

---
+++ src/main/java/com/utp/users/model/entity/UserRole.java
@@ -0,0 +1,23 @@
+package com.utp.users.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table("user_roles")
+public class UserRole {
+    @Id
+    private Long id;
+
+    @Column("id_role")
+    private Long idRole;
+
+    @Column("id_user")
+    private Long idUser;
+}
